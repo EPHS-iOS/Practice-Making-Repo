dee80084fd625b754a74fcda2de66cefa0b7f003
RohitP123 <90308603@ep-student.org>
2022-02-15 10:16:52 -0600

    Initial Commit

---
+++ FunTime/FunTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunTimeApp.swift
+//  FunTime
+//
+//  Created by 90308603 on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct FunTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
